3de8dedf0ee1075b4980ecee3b5de0de293982f1
[Majid Syed] <[smajidkhalil@gmail.com]>
2024-03-09 14:04:51 +0500

    first commit

---
+++ assignment 1/main.ts
@@ -0,0 +1,9 @@
+// Assignment # 1
+
+// storing the person name in a variable
+
+let personName:string = "Hina"
+
+// printing a message to the person
+
+console.log(`Hello ${personName} would you like to learn some python today?`)
