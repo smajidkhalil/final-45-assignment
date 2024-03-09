d73f27432388524816220e5eaa407ee06c7fb8df
[Majid Syed] <[smajidkhalil@gmail.com]>
2024-03-09 14:09:56 +0500

    first commit

---
+++ assginment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Nothing is impossible becuase impossible itself I M POSSIBLE";
+let author = "Hina Naseer";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assginment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impossible becuase impossible itself I M POSSIBLE"
+
+let author:string =  "Hina Naseer"
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
